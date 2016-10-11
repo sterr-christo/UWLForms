36ee16a37f396b8f477233bd33cdc87ba0fc91d0
c <sterr.christo@gmail.com>
2016-10-11 09:43:56 -0500

    Initial Commit

---
+++ test.js
@@ -0,0 +1,10 @@
+var studentName = document.getElementById("studentName")
+var studentID = document.getElementById("studentID")
+var studentPhone = document.getElementById("studentPhone")
+var studentGPA = document.getElementByID("studentGPA")
+var btnSubmit = document.getElementByID("btnSubmit")
+btnSubmit.addEventListener("click", submit);
+
+function submit() {
+
+}
